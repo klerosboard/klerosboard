de92eb8473a742938abae0cd4bf7b19e1ff74398
Algozino Santiago <salgozino@gmail.com>
2022-10-03 01:09:06 -0300

    fix: chainId in courts

--- src/pages/Courts.tsx
+++ src/pages/Courts.tsx
@@ -1,55 +1,41 @@
 import React, { useState } from 'react'
 import Header from '../components/Header';
 import { useCourts } from '../hooks/useCourts'
-import {
-  DataGrid, 
-} from '@mui/x-data-grid'
-import { CustomFooter } from '../components/DataGridFooter'
+import { DataGrid } from '@mui/x-data-grid'
 import { Link } from '@mui/material';
-import { Link as LinkRouter, useSearchParams } from 'react-router-dom';
-import { formatAmount, formatPNK, getChainId, getCourtName } from '../lib/helpers';
+import { Link as LinkRouter,  useSearchParams } from 'react-router-dom';
+import { formatAmount, formatPNK, getChainId, getCourtName, getCurrency } from '../lib/helpers';
 import { BigNumberish, ethers } from 'ethers';
-
-
-
 
 export default function Courts() {
   let [searchParams] = useSearchParams();
   const chainId = getChainId(searchParams);
-  const { data: courts, isLoading } = useCourts();
+  const { data: courts, isLoading } = useCourts(chainId);
+
   const [pageSize, setPageSize] = useState<number>(10);
 
   const columns = [
-    { field: 'id', headerName: 'Court Id', flex: 1, valueFormatter: (params: { value: BigNumberish }) => { return Number(params.value) } },
-    {
-      field: 'name', headerName: 'Court Name', flex: 2, renderCell: (params: { id: BigNumberish }) => {
-        return (<Link component={LinkRouter} to={'/courts/' + params.id} children={params.id} />);
-      }
+    { field: 'id', headerName: 'Court Id', flex: 1, valueFormatter: (params:{value:BigNumberish}) => {return Number(params.value)}},
+    { field: 'name', headerName: 'Court Name', flex: 2, renderCell: (params: {row: {id: string}}) => {
+      return getCourtName(params.row.id, chainId);
+    }
     },
-    {
-      field: 'tokenStaked', headerName: 'Total Staked', flex: 1, valueFormatter: (params: { value: BigNumberish }) => {
-        const valueFormatted = Number(params.value).toLocaleString(undefined, { maximumFractionDigits: 0 });
-        return `${valueFormatted}`;
-      }
-    },
-    { field: 'activeJurors', headerName: 'Active Jurors', flex: 1, valueFormatter: (params: { value: BigNumberish }) => { return Number(params.value) } },
-    {
-      field: 'feeForJuror', headerName: 'Fee for Jurors', flex: 1, valueFormatter: (params: { value: BigNumberish }) => {
-        return formatAmount(params.value, chainId);
-      }
-    },
-    {
-      field: 'minStake', headerName: 'Min Stake', flex: 1, valueFormatter: (params: { value: BigNumberish }) => {
-        return formatPNK(params.value);
-      }
-    },
-    {
-      field: 'voteStake', headerName: 'Vote Stake', flex: 1, renderCell: (params: { row: { minStake: BigNumberish, alpha: BigNumberish } }) => {
-        return ((Number(ethers.utils.formatUnits(params.row.minStake, 'ether')) * Number(params.row.alpha) / 10000).toLocaleString() + ' PNK');
-      }
-    },
-    { field: 'disputesNum', headerName: 'Total Disputes', flex: 1, valueFormatter: (params: { value: BigNumberish }) => { return Number(params.value) } },
-    { field: 'disputesOngoing', headerName: 'Open Disputes', flex: 1, valueFormatter: (params: { value: BigNumberish }) => { return Number(params.value) } },
+    { field: 'tokenStaked', headerName: 'Total Staked', flex: 1, valueFormatter: (params:{value:BigNumberish}) => {
+      const valueFormatted = Number(params.value).toLocaleString(undefined, { maximumFractionDigits: 0 });
+      return `${valueFormatted}`;
+    }},
+    { field: 'activeJurors', headerName: 'Active Jurors', flex: 1, valueFormatter: (params:{value:BigNumberish}) => {return Number(params.value)}},
+    { field: 'feeForJuror', headerName: 'Fee for Jurors', flex: 1, valueFormatter: (params:{value:BigNumberish}) => {
+      return formatAmount(params.value, chainId);
+    }},
+    { field: 'minStake', headerName: 'Min Stake', flex: 1, valueFormatter: (params:{value:BigNumberish}) => {
+      return formatPNK(params.value);
+    }},
+    { field: 'voteStake', headerName: 'Vote Stake', flex: 1, renderCell: (params: {row: {minStake: BigNumberish, alpha: BigNumberish}}) => {
+      return ((Number(ethers.utils.formatUnits(params.row.minStake, 'ether')) * Number(params.row.alpha) / 10000).toLocaleString() + ' PNK');
+    }},
+    { field: 'disputesNum', headerName: 'Total Disputes', flex: 1, valueFormatter: (params:{value:BigNumberish}) => {return Number(params.value)}},
+    { field: 'disputesOngoing', headerName: 'Open Disputes', flex: 1, valueFormatter: (params:{value:BigNumberish}) => {return Number(params.value)}},
   ];
 
 
@@ -60,10 +46,10 @@ export default function Courts() {
         title='Courts'
         text='Learn more about the courts, stakes, jurors and other stats'
       />
-
-
+      
+      
       {<DataGrid
-        rows={courts ? courts! : []}
+        rows={courts? courts! : []}
         columns={columns}
         loading={isLoading}
         pageSize={pageSize}
@@ -72,11 +58,8 @@ export default function Courts() {
         pagination
         disableSelectionOnClick
         autoHeight={true}
-        components={{
-          Footer: CustomFooter
-        }}
       />}
-
+    
     </div>
   )
 }
