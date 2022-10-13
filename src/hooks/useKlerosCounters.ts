748608dd4e61160ef9612fbeaa35c3f48dc36721
Algozino Santiago <salgozino@gmail.com>
2022-10-13 12:56:10 -0300

    chore: add adoption metrics

--- src/hooks/useKlerosCounters.ts
+++ src/hooks/useKlerosCounters.ts
@@ -1,6 +1,8 @@
 import { KLEROSCOUNTERS_FIELDS, KlerosCounter } from "../graphql/subgraph";
 import { useQuery } from "@tanstack/react-query";
 import { apolloClientQuery } from "../lib/apolloClient";
+import { getBlockByDate } from "../lib/helpers";
+import { ApolloQueryResult } from "@apollo/client";
 
 const query = `
     ${KLEROSCOUNTERS_FIELDS}
@@ -11,14 +13,36 @@ const query = `
     }
 `;
 
-export const useKlerosCounter = (chainId: string = '1') => {
-  return useQuery<KlerosCounter, Error>(
-    ["useklerosCounter", chainId],
-    async () => {      
-      const response = await apolloClientQuery<{ klerosCounter: KlerosCounter }>(chainId, query);
+const queryRel = `
+    ${KLEROSCOUNTERS_FIELDS}
+    query KCQuery($blockNumber: Int!) {
+      klerosCounter(id:"ID", block:{number:$blockNumber}) {
+        ...KlerosCountersFields
+      }
+    }
+`;
 
+interface Props {
+  chainId: string
+  relTimestamp?: string | Date
+}
+
+export const useKlerosCounter = ({ chainId, relTimestamp }: Props) => {
+  return useQuery<KlerosCounter, Error>(
+    ["useklerosCounter", chainId, relTimestamp],
+    async () => {
+
+      let response: ApolloQueryResult<{ klerosCounter: KlerosCounter }> | undefined
+      if (relTimestamp) {
+        const blockNumber = (await getBlockByDate(relTimestamp)).block;
+
+        if (!blockNumber) throw new Error("No response from Infura");;
+        response = await apolloClientQuery<{ klerosCounter: KlerosCounter }>(chainId, queryRel, { blockNumber: blockNumber });
+      } else {
+        response = await apolloClientQuery<{ klerosCounter: KlerosCounter }>(chainId, query);
+      }
       if (!response) throw new Error("No response from TheGraph");
-      
+
       return response.data.klerosCounter;
     }
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSearchParams } from 'react-router-dom';
-import { Grid, Skeleton } from '@mui/material';
+import { Grid, Skeleton, Typography } from '@mui/material';
 import { useKlerosCounter } from '../hooks/useKlerosCounters'
 import { formatAmount, formatPNK, getChainId } from '../lib/helpers';
-import {subDays } from 'date-fns';
+import { subDays } from 'date-fns';
 
 import Header from '../components/Header';
 import StatCard from '../components/StatCard';
@@ -17,7 +17,8 @@ import REWARD_UP from '../assets/icons_stats/reward_up.png';
 import REWARD from '../assets/icons_stats/reward.png';
 import COMMUNITY from '../assets/icons_stats/community_green.png';
 import COMMUNITY_NO_CIRCLE from '../assets/icons_stats/community_no_circle.png';
-import ARROW_UP from '../assets/icons_stats/arrow_up_violet.png';
+import ARROW_UP from '../assets/icons/arrow_up_violet.png';
+import ARROW_DOWN from '../assets/icons/arrow_down_violet.png';
 import KLEROS from '../assets/icons_stats/kleros.png';
 import KLEROS_ORACLE from '../assets/icons_stats/kleros_oracle.png';
 import KLEROS_ARROWS from '../assets/icons_stats/kleros_arrows.png';
@@ -30,20 +31,35 @@ import { BigNumberish, ethers } from 'ethers';
 import { useMostActiveCourt } from '../hooks/useMostActiveCourt';
 import CourtLink from '../components/CourtLink';
 import { useCourts } from '../hooks/useCourts';
-import { Court } from '../graphql/subgraph';
+import { Court, KlerosCounter } from '../graphql/subgraph';
 
 
 const row_css = {
   justifyContent: 'space-between',
   alignItems: 'center',
-  border:  '1px solid #E5E5E5',
+  border: '1px solid #E5E5E5',
   boxShadow: '0px 2px 3px rgba(0, 0, 0, 0.06)',
-  borderRadius:'3px',
+  borderRadius: '3px',
   margin: '10px 0px',
   paddingTop: '0px'
 }
 
-function stakingReward(chainId:string, totalStaked:BigNumberish | undefined): number {
+const blackText = {
+  fontStyle: 'normal',
+  fontWeight: 600,
+  fontSize: '14px',
+  lineHeight: '19px',
+  color: '#333333'
+}
+
+const grayText = {
+  fontStyle: 'normal',
+  fontWeight: 400,
+  fontSize: '14px',
+  lineHeight: '19px',
+}
+
+function stakingReward(chainId: string, totalStaked: BigNumberish | undefined): number {
 
   if (chainId === '100') {
     if (totalStaked) return 100000 / Number(ethers.utils.formatUnits(totalStaked, 'ether')) * 100 * 12
@@ -52,24 +68,36 @@ function stakingReward(chainId:string, totalStaked:BigNumberish | undefined): nu
   return 0
 }
 
-function getMaxReward(courts: Court[]):Court {
+function getMaxReward(courts: Court[]): Court {
   return courts.reduce((a, b) => Number(a.feeForJuror) > Number(b.feeForJuror) ? a : b)
 }
 
-function getMaxChance(courts: Court[]):Court {
+function getMaxChance(courts: Court[]): Court {
   return courts.reduce((a, b) => Number(a.tokenStaked) > Number(b.feeForJuror) ? b : a)
+}
+
+function getJurorsGrowth(kc: KlerosCounter, kcOld: KlerosCounter) {
+  return Number(kcOld.activeJurors) - Number(kc.activeJurors)
 }
 
 export default function Home() {
   let [searchParams] = useSearchParams();
   const chainId = getChainId(searchParams)
-  const { data: kc } = useKlerosCounter(chainId);
-  const {data: mostActiveCourt} = useMostActiveCourt({chainId:chainId});
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [relativeDate, setRelativeDate] = useState<Date>(new Date())  // To avoid refetching the query
-  const {data: mostActiveCourtRelative} = useMostActiveCourt({chainId:chainId, relTimestamp:subDays(relativeDate, 7)});
-  const {data: courts} = useCourts({chainId: chainId})
-  
+  const [jurorAdoption, setJurorAdoption] = useState<number | undefined>(undefined)
+  const { data: kc } = useKlerosCounter({ chainId: chainId });
+  const { data: kcOld } = useKlerosCounter({ chainId: chainId, relTimestamp: subDays(relativeDate, 7) });
+  const { data: mostActiveCourt } = useMostActiveCourt({ chainId: chainId });
+  const { data: mostActiveCourtRelative } = useMostActiveCourt({ chainId: chainId, relTimestamp: subDays(relativeDate, 7) });
+  const { data: courts } = useCourts({ chainId: chainId })
+
+  useEffect(() => {
+    if (kc && kcOld) {
+      setJurorAdoption(getJurorsGrowth(kc, kcOld))
+    }
+  }, [kc, kcOld])
+
   return (
     <div>
       <Header
@@ -79,15 +107,15 @@ export default function Home() {
       />
       <Grid container justifyContent='center' alignItems='start'>
         <Grid container item columnSpacing={0} sx={row_css}>
-          <Grid item xs={12} md={4} lg={3}><StatCard title={'Most Active Court'} subtitle={'All times'} value={mostActiveCourt?<CourtLink chainId={chainId} courtId={mostActiveCourt.id} />:<Skeleton />} image={BALANCE} /></Grid>
-          <Grid item xs={12} md={4} lg={3}><StatCard title={'Most Active Court'} subtitle={'Last 7 days'} value={mostActiveCourtRelative?<CourtLink chainId={chainId} courtId={mostActiveCourtRelative.id} />:<Skeleton />} image={BALANCE} /></Grid>
-          <Grid item xs={12} md={4} lg={3}><StatCard title={'Highest Draw Chance'} subtitle={'All times'} value={courts?<CourtLink chainId={chainId} courtId={getMaxChance(courts).id} />:<Skeleton />} image={DICE} /></Grid>
-          <Grid item xs={12} md={4} lg={3}><StatCard title={'Highest reward chance'} subtitle={'All times'} value={courts?<CourtLink chainId={chainId} courtId={getMaxReward(courts).id} />:<Skeleton />} image={REWARD_UP} /></Grid>
+          <Grid item xs={12} md={4} lg={3}><StatCard title={'Most Active Court'} subtitle={'All times'} value={mostActiveCourt ? <CourtLink chainId={chainId} courtId={mostActiveCourt.id} /> : <Skeleton />} image={BALANCE} /></Grid>
+          <Grid item xs={12} md={4} lg={3}><StatCard title={'Most Active Court'} subtitle={'Last 7 days'} value={mostActiveCourtRelative ? <CourtLink chainId={chainId} courtId={mostActiveCourtRelative.id} /> : <Skeleton />} image={BALANCE} /></Grid>
+          <Grid item xs={12} md={4} lg={3}><StatCard title={'Highest Draw Chance'} subtitle={'All times'} value={courts ? <CourtLink chainId={chainId} courtId={getMaxChance(courts).id} /> : <Skeleton />} image={DICE} /></Grid>
+          <Grid item xs={12} md={4} lg={3}><StatCard title={'Highest reward chance'} subtitle={'All times'} value={courts ? <CourtLink chainId={chainId} courtId={getMaxReward(courts).id} /> : <Skeleton />} image={REWARD_UP} /></Grid>
         </Grid>
         <Grid container item columnSpacing={0} sx={row_css}>
-          <Grid item xs={12} md={4} lg={2}><StatCard title={'PNK Staked'} subtitle={'All times'} value={kc? formatPNK(kc.tokenStaked) : undefined} image={KLEROS} /></Grid>
-          <Grid item xs={12} md={4} lg={2}><StatCard title={'ETH Paid'} subtitle={'All times'} value={kc? formatAmount(kc.totalETHFees, chainId): undefined} image={ETHEREUM} /></Grid>
-          <Grid item xs={12} md={4} lg={2}><StatCard title={'PNK Redistributed'} subtitle={'All times'} value={kc? formatPNK(kc.totalTokenRedistributed) : undefined} image={KLEROS_ORACLE} /></Grid>
+          <Grid item xs={12} md={4} lg={2}><StatCard title={'PNK Staked'} subtitle={'All times'} value={kc ? formatPNK(kc.tokenStaked) : undefined} image={KLEROS} /></Grid>
+          <Grid item xs={12} md={4} lg={2}><StatCard title={'ETH Paid'} subtitle={'All times'} value={kc ? formatAmount(kc.totalETHFees, chainId) : undefined} image={ETHEREUM} /></Grid>
+          <Grid item xs={12} md={4} lg={2}><StatCard title={'PNK Redistributed'} subtitle={'All times'} value={kc ? formatPNK(kc.totalTokenRedistributed) : undefined} image={KLEROS_ORACLE} /></Grid>
           <Grid item xs={12} md={4} lg={2}><StatCard title={'Active Jurors'} subtitle={'All times'} value={kc?.activeJurors} image={COMMUNITY} /></Grid>
           <Grid item xs={12} md={4} lg={2}><StatCard title={'Cases'} subtitle={'All times'} value={kc?.disputesCount} image={BALANCE} /></Grid>
         </Grid>
@@ -98,10 +126,21 @@ export default function Home() {
           <Grid item xs={12} md={4} lg={2}><StatCard title={'PNK Price'} subtitle={'ETH = $1,258.15'} value={'$ .05'} image={KLEROS} /></Grid>
           <Grid item xs={12} md={4} lg={2}><StatCard title={'Staking Rewards'} subtitle={'APY'} value={`${stakingReward(chainId, kc?.tokenStaked).toFixed(2)}%`} image={REWARD} /></Grid>
         </Grid>
-        <Grid container item columnSpacing={0} justifyContent='center' alignContent='center'>
-          <Grid item xs={3}><img height={'14px'} src={COMMUNITY_NO_CIRCLE} alt={'Community logo'} style={{marginRight: '15px'}}/>Jurors' growth: {'123'}</Grid>
-          <Grid item xs={3}><img height={'14px'} src={ARROW_UP} alt={'Arrow'} style={{marginRight: '15px'}}/>Adoption: {'123'} new jurors</Grid>
-          <Grid item xs={3}><img height={'14px'} src={ARROW_UP} alt={'Arrow'} style={{marginRight: '15px'}}/>Retention {'33%'}</Grid>
+        <Grid container item columnSpacing={0} justifyContent='center' alignItems='center'>
+          <Grid item xs={3} display='flex' alignItems='center'>
+            <img height={'14px'} src={COMMUNITY_NO_CIRCLE} alt={'Community logo'} style={{ marginRight: '15px' }} />
+            <Typography  sx={blackText}>Jurors' growth (last month): </Typography>
+          </Grid>
+          <Grid item xs={3} alignItems='center' display='inline-flex'>
+            <img height={'14px'} src={jurorAdoption && jurorAdoption < 0 ? ARROW_DOWN : ARROW_UP} alt={'Arrow'} style={{ marginRight: '15px' }} />
+            <Typography sx={grayText}>Adoption:&nbsp;</Typography>
+            <Typography sx={blackText} display='flex'>{jurorAdoption ? jurorAdoption : <Skeleton variant='circular' width={'10px'} />} new jurors</Typography>
+          </Grid>
+          <Grid item xs={3} alignItems='center' display='inline-flex'>
+            <img height={'14px'} src={jurorAdoption && jurorAdoption < 0 ? ARROW_DOWN : ARROW_UP} alt={'Arrow'} style={{ marginRight: '15px' }} />
+            <Typography sx={grayText}>Retention:&nbsp;</Typography>
+            <Typography sx={blackText} display='flex'>{jurorAdoption ? (jurorAdoption! / Number(kcOld!.activeJurors) * 100).toFixed(2) + '%' : <Skeleton variant='circular' width={'10px'}/>}</Typography>
+          </Grid>
         </Grid>
       </Grid>
 
@@ -112,7 +151,7 @@ export default function Home() {
         </Grid>
 
         <Grid item xs={12} md={6}>
-          <LatestDisputes chainId={chainId} courtId={undefined}/>
+          <LatestDisputes chainId={chainId} courtId={undefined} />
         </Grid>
 
       </Grid>
