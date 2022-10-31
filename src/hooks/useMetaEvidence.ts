609482724f7f55c72c0d1a88ae5e79f576ae76dc
Algozino Santiago <salgozino@gmail.com>
2022-10-30 22:41:56 -0300

    fix archon and useMetaEvidence

--- src/hooks/useMetaEvidence.ts
+++ src/hooks/useMetaEvidence.ts
@@ -1,8 +1,36 @@
-import { getArchon } from "../lib/archonClient"
+import { useEffect, useState } from 'react'
+import { getArchon } from "../lib/archonClient";
+import { GNOSIS_KLEROSLIQUID, MAINNET_KLEROSLIQUID } from "../lib/helpers";
 import { MetaEvidence } from "../lib/types";
 
 
-export const useMetaEvidence = (chainId: string = '1', arbitrableId:string, disputeId:string):Promise<MetaEvidence> => {
-    const archon = getArchon(chainId);
-    return archon.arbitrable.getMetaEvidence(arbitrableId, disputeId);
+export const useMetaEvidence = (chainId: string = '1', arbitrableId: string|undefined, disputeId: string): MetaEvidence | undefined => {
+  const [metaEvidence, setMetaEvidence] = useState<undefined | MetaEvidence>(undefined)
+  const archon = getArchon(chainId);
+  const KL = chainId === '100' ? GNOSIS_KLEROSLIQUID : MAINNET_KLEROSLIQUID;
+
+  useEffect(() => {
+    async function fetchMetaevidence() {
+      const response = await archon.arbitrable.getDispute(arbitrableId, KL, disputeId);
+
+      const _metaEvidence: MetaEvidence = await archon.arbitrable.getMetaEvidence(arbitrableId, response.metaEvidenceID, {
+        strict: true,
+        scriptParameters: {
+          disputeID: disputeId,
+          arbitrableContractAddress: arbitrableId,
+          arbitratorContractAddress: KL,
+          arbitratorChainID: chainId,
+          chainID: chainId,
+          // arbitratorJsonRpcUrl: networkMap[this.state.network].WEB3_PROVIDER,
+        },
+      });
+
+      setMetaEvidence(_metaEvidence);
+    }
+    if (arbitrableId && disputeId) {
+      fetchMetaevidence()
+    }
+  }, [arbitrableId, chainId, KL, disputeId, archon, metaEvidence])
+
+  return metaEvidence
 }
