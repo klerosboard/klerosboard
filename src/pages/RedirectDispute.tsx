704e8c6ec0d3468ed7375777e0a2e85dc830646c
Algozino Santiago <santiago.algozino@satellogic.com>
2023-03-04 19:12:55 -0300

    support old disputes links and close #21

---
+++ src/pages/RedirectDispute.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Navigate, useSearchParams } from "react-router-dom";
+
+
+export default function RedirectDispute() {
+    let [searchParams,] = useSearchParams();
+    const disputeId = searchParams.get('id');
+    const network = searchParams.get('network');
+    let chainId = 1;
+    if (network === 'xdai') {
+        chainId = 100;
+    }
+    console.log()
+
+    if (disputeId !== undefined && disputeId !== null) {
+        return (
+        <Navigate to={`/${chainId}/cases/${disputeId}`} replace />
+        )
+    }
+    return (
+        <Navigate to={`/${chainId}/cases`} replace />
+    )
+}
