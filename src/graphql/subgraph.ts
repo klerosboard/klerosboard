601b986c2f0254bcc6557737818d981dcfb0e6a4
salgozino <salgozino@gmail.com>
2023-08-03 14:35:25 -0300

    add appeal count, court coherency and appeal %

--- src/graphql/subgraph.ts
+++ src/graphql/subgraph.ts
@@ -242,16 +242,16 @@ export interface Court {
     disputesCount: BigNumberish
     openDisputes: BigNumberish
     closedDisputes: BigNumberish
-    // evidencePhaseDisputes: BigNumberish
-    // commitPhaseDisputes: BigNumberish
-    // votingPhaseDisputes: BigNumberish
-    // appealPhaseDisputes: BigNumberish
+    evidencePhaseDisputes: BigNumberish
+    commitPhaseDisputes: BigNumberish
+    votingPhaseDisputes: BigNumberish
+    appealPhaseDisputes: BigNumberish
     ethFees: BigNumberish
     activeJurors: BigNumberish
     disputesNum: BigNumberish
     disputesClosed: BigNumberish
     disputesOngoing: BigNumberish
-    // disputesAppealed: BigNumberish
+    disputesAppealed: BigNumberish
     feeForJuror: BigNumberish
     minStake: BigNumberish
     alpha: BigNumberish
@@ -262,8 +262,8 @@ export interface Court {
     totalETHFees: BigNumberish
     totalTokenRedistributed: BigNumberish
     name: string
-    // coherency: BigNumberish
-    // appealPercentage: BigNumberish
+    coherency: BigNumberish
+    appealPercentage: BigNumberish
 }
 
 export const COURT_FIELDS = `
@@ -286,6 +286,13 @@ export const COURT_FIELDS = `
         timePeriods
         totalETHFees
         totalTokenRedistributed
+        coherency
+        appealPercentage
+        evidencePhaseDisputes
+        commitPhaseDisputes
+        votingPhaseDisputes
+        appealPhaseDisputes
+        disputesAppealed
     }
 `;
 
