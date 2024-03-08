090289714f4a3eaaf966c4fc99e3e58927341c63
salgozino <salgozino@gmail.com>
2024-03-08 10:34:13 -0300

    [chore] set default chain as mainnet in links if chain not found

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -101,7 +101,7 @@ export default function Layout() {
   const theme = useTheme();
   const location = useLocation();
   const match = location.pathname.match('(100|1)(?:/|$)')
-  const chainId = match ? match[1] : null
+  const chainId = match ? match[1] : '1'
 
   const toggleDrawer = () => {
     setOpen(!open);
