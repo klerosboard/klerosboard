f50bca0b3fb608a016136267f759aed16349d328
salgozino <salgozino@gmail.com>
2024-05-01 16:13:00 -0300

    fix: value exported to CSV instead of [Object]

--- src/components/LatestStakes.tsx
+++ src/components/LatestStakes.tsx
@@ -1,101 +1,165 @@
-import React from 'react'
-import { Box, Typography } from '@mui/material';
-import { DataGrid, GridRenderCellParams } from '@mui/x-data-grid';
-import { shortenAddress } from '@usedapp/core';
-import { BigNumberish } from 'ethers';
-import { Juror } from '../graphql/subgraph';
-import { useStakes } from '../hooks/useStakes';
-import CourtLink from './CourtLink';
-import { Link as LinkRouter } from 'react-router-dom';
-import { Link } from '@mui/material';
-import { formatDate, formatPNK } from '../lib/helpers';
-import { CustomFooter } from './DataGridFooter';
-
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import { DataGrid, GridRenderCellParams, GridValueFormatterParams } from "@mui/x-data-grid";
+import { shortenAddress } from "@usedapp/core";
+import { BigNumberish } from "ethers";
+import { Juror } from "../graphql/subgraph";
+import { useStakes } from "../hooks/useStakes";
+import CourtLink from "./CourtLink";
+import { Link as LinkRouter } from "react-router-dom";
+import { Link } from "@mui/material";
+import { formatDate, formatPNK } from "../lib/helpers";
+import { CustomFooter } from "./DataGridFooter";
 
 interface Props {
-    chainId: string
-    courtId?: string
-    jurorId?: string
-    hideFooter?: boolean
+  chainId: string;
+  courtId?: string;
+  jurorId?: string;
+  hideFooter?: boolean;
 }
 
 export default function LatestStakes(props: Props) {
-    const { data: stakes, isLoading: stakes_loading } = useStakes({chainId: props.chainId, subcourtID: props.courtId, jurorID: props.jurorId});
-    const columns_stakes = [
-        {
-          field: 'address', headerName: 'Juror', flex: 1, renderCell: (params: GridRenderCellParams<Juror>) => (
-            <Link component={LinkRouter} to={`/${props.chainId}/profile/${params.value!.id}`} children={shortenAddress(params.value!.id)} />
-          )
-        },
-        {
-          field: 'subcourtID', headerName: 'Court Name', flex: 2, renderCell: (params: GridRenderCellParams<BigNumberish>) => (
-            <CourtLink chainId={props.chainId} courtId={params.value! as string} />
-          )
-        },
-        {
-          field: 'stake', headerName: 'Last Stake',  type:'number', flex: 1, valueFormatter: (params: { value: BigNumberish }) => {
-            return formatPNK(params.value);
-          }
-        },
-      ];
-      const columns_stakes_wihtout_court = [
-        {
-          field: 'address', headerName: 'Juror', flex: 1, renderCell: (params: GridRenderCellParams<Juror>) => (
-            <Link component={LinkRouter} to={`/${props.chainId}/profile/${params.value!.id}`} children={shortenAddress(params.value!.id)} />
-          )
-        },
-        {
-          field: 'stake', headerName: 'Stake', type:'number', flex: 1, valueFormatter: (params: { value: BigNumberish }) => {
-            return formatPNK(params.value);
-          }
-        },
-        {
-          field: 'timestamp', headerName: 'Date', type:'number', flex: 1, valueFormatter: (params: { value: BigNumberish }) => {
-            return formatDate(Number(params.value));
-          }
-        },
-      ];
+  const { data: stakes, isLoading: stakes_loading } = useStakes({
+    chainId: props.chainId,
+    subcourtID: props.courtId,
+    jurorID: props.jurorId,
+  });
+  const columns_stakes = [
+    {
+      field: "address",
+      headerName: "Juror",
+      flex: 1,
+      valueFormatter: (params: GridValueFormatterParams) => `${params.value.id}`,
+      renderCell: (params: GridRenderCellParams<Juror>) => (
+        <Link
+          component={LinkRouter}
+          to={`/${props.chainId}/profile/${params.value!.id}`}
+          children={shortenAddress(params.value!.id)}
+        />
+      )
+    },
+    {
+      field: "subcourtID",
+      headerName: "Court Name",
+      flex: 2,
+      renderCell: (params: GridRenderCellParams<BigNumberish>) => (
+        <CourtLink chainId={props.chainId} courtId={params.value! as string} />
+      ),
+    },
+    {
+      field: "stake",
+      headerName: "Last Stake",
+      type: "number",
+      flex: 1,
+      valueFormatter: (params: GridValueFormatterParams) => {
+        console.log(params.value)
+        return formatPNK(params.value);
+      },
+    },
+  ];
+  const columns_stakes_wihtout_court = [
+    {
+      field: "address",
+      headerName: "Juror",
+      flex: 1,
+      valueFormatter: (params: GridValueFormatterParams) => `${params.value.id}`,
+      renderCell: (params: GridRenderCellParams<Juror>) => (
+        <Link
+          component={LinkRouter}
+          to={`/${props.chainId}/profile/${params.value!.id}`}
+          children={shortenAddress(params.value!.id)}
+        />
+      )      
+    },
+    {
+      field: "stake",
+      headerName: "Stake",
+      type: "number",
+      flex: 1,
+      valueFormatter: (params: GridValueFormatterParams) => {
+        return formatPNK(params.value);
+      },
+    },
+    {
+      field: "timestamp",
+      headerName: "Date",
+      type: "number",
+      flex: 1,
+      valueFormatter: (params: GridValueFormatterParams) => {
+        return formatDate(Number(params.value));
+      },
+    },
+  ];
 
-      const columns_stakes_for_juror = [
-        {
-          field: 'subcourtID', headerName: 'Court Name', flex: 2, renderCell: (params: GridRenderCellParams<BigNumberish>) => (
-            <CourtLink chainId={props.chainId} courtId={params.value! as string} />
-          )
-        },
-        {
-          field: 'stake', headerName: 'Stake', type:'number', flex: 1, valueFormatter: (params: { value: BigNumberish }) => {
-            return formatPNK(params.value);
-          }
-        },
-        {
-          field: 'newTotalStake', headerName: 'Total in Courts', type:'number', flex: 1, valueFormatter: (params: { value: BigNumberish }) => {
-            return formatPNK(params.value);
-          }
-        },
-        {
-          field: 'timestamp', headerName: 'Date', type:'number', flex: 1, valueFormatter: (params: { value: BigNumberish }) => {
-            return formatDate(Number(params.value));
-          }
-        },
-      ];
+  const columns_stakes_for_juror = [
+    {
+      field: "subcourtID",
+      headerName: "Court Name",
+      flex: 2,
+      renderCell: (params: GridRenderCellParams<BigNumberish>) => (
+        <CourtLink chainId={props.chainId} courtId={params.value! as string} />
+      ),
+      valueFormatter: (params: GridValueFormatterParams) => {
+        return `${params.value}`
+      }
+    },
+    {
+      field: "stake",
+      headerName: "Stake",
+      type: "number",
+      flex: 1,
+      valueFormatter: (params: GridValueFormatterParams) => {
+        return formatPNK(params.value);
+      },
+    },
+    {
+      field: "newTotalStake",
+      headerName: "Total in Courts",
+      type: "number",
+      flex: 1,
+      valueFormatter: (params: GridValueFormatterParams) => {
+        return formatPNK(params.value);
+      },
+    },
+    {
+      field: "timestamp",
+      headerName: "Date",
+      type: "number",
+      flex: 1,
+      valueFormatter: (params: GridValueFormatterParams) => {
+        return formatDate(Number(params.value));
+      },
+    },
+  ];
 
-    return (
-        <Box>
-            <Typography sx={{ fontSize: '24px', fontWeight: 600, fontStyle: 'normal' }}>Latest Stakes</Typography>
-            {<DataGrid
-                sx={{ marginTop: '30px' }}
-                rows={stakes ? stakes! : []}
-                columns={props.courtId ? columns_stakes_wihtout_court: props.jurorId? columns_stakes_for_juror : columns_stakes}
-                loading={stakes_loading}
-                pageSize={10}
-                disableSelectionOnClick
-                autoHeight={true}
-                hideFooter={props.hideFooter === undefined? true: props.hideFooter}
-                components={{
-                  Footer: CustomFooter
-                }}
-            />}
-        </Box>
-
-    )
+  return (
+    <Box>
+      <Typography
+        sx={{ fontSize: "24px", fontWeight: 600, fontStyle: "normal" }}
+      >
+        Latest Stakes
+      </Typography>
+      {
+        <DataGrid
+          sx={{ marginTop: "30px" }}
+          rows={stakes ? stakes! : []}
+          columns={
+            props.courtId
+              ? columns_stakes_wihtout_court
+              : props.jurorId
+              ? columns_stakes_for_juror
+              : columns_stakes
+          }
+          loading={stakes_loading}
+          pageSize={10}
+          disableSelectionOnClick
+          autoHeight={true}
+          hideFooter={props.hideFooter === undefined ? true : props.hideFooter}
+          components={{
+            Footer: CustomFooter,
+          }}
+        />
+      }
+    </Box>
+  );
 }
--- src/components/Profile/CreatedCases.tsx
+++ src/components/Profile/CreatedCases.tsx
@@ -1,64 +1,103 @@
-import { Box, Skeleton, Typography } from '@mui/material';
-import { DataGrid, GridRenderCellParams } from '@mui/x-data-grid';
-import { BigNumberish } from 'ethers';
-import React from 'react'
-import { formatDate, getBlockExplorer } from '../../lib/helpers';
-import CourtLink from '../CourtLink';
-import { Link } from '@mui/material';
-import { Link as LinkRouter } from 'react-router-dom';
-import { Court, Dispute } from '../../graphql/subgraph';
-import { CustomFooter } from '../DataGridFooter';
+import { Box, Skeleton, Typography } from "@mui/material";
+import { DataGrid, GridRenderCellParams, GridValueFormatterParams } from "@mui/x-data-grid";
+import { BigNumberish } from "ethers";
+import React from "react";
+import { formatDate, getBlockExplorer } from "../../lib/helpers";
+import CourtLink from "../CourtLink";
+import { Link } from "@mui/material";
+import { Link as LinkRouter } from "react-router-dom";
+import { Court, Dispute } from "../../graphql/subgraph";
+import { CustomFooter } from "../DataGridFooter";
 
 interface Props {
-    cases: Dispute[] | undefined
-    chainId: string
-    isLoading: boolean
+  cases: Dispute[] | undefined;
+  chainId: string;
+  isLoading: boolean;
 }
 
 export default function CreatedCases(props: Props) {
-    const blockExplorer = getBlockExplorer(props.chainId);
+  const blockExplorer = getBlockExplorer(props.chainId);
 
-    const dispute_columns = [
-        {
-            field: 'id', headerName: '#', flex: 1, renderCell: (params: GridRenderCellParams<Court>) => (
-                <Link component={LinkRouter} to={`/${props.chainId}/cases/${params.value}`} children={params.value} />
-            )
-        },
-        {
-            field: 'subcourtID', headerName: 'Court', flex: 2, renderCell: (params: GridRenderCellParams<Court>) => (
-                <CourtLink chainId={props.chainId} courtId={params.value!.id as string} />
-            )
-        },
-        {
-            field: 'startTime', headerName: 'Date', flex: 2, renderCell: (params: GridRenderCellParams<BigNumberish>) => (
-                formatDate(Number(params.value!))
-            )
-        },
-        {
-            field: 'txid', headerName: 'txID', flex: 1, renderCell: (params: GridRenderCellParams<string>) => (
-                <a href={`${blockExplorer}/txid/${params.value}`} rel='noreferrer' target='_blank'>{`${params.value?.slice(0, 6)}...${params.value?.slice(-4)}`}</a>
-            )
+  const dispute_columns = [
+    {
+      field: "id",
+      headerName: "#",
+      flex: 1,
+      renderCell: (params: GridRenderCellParams<Court>) => (
+        <Link
+          component={LinkRouter}
+          to={`/${props.chainId}/cases/${params.value}`}
+          children={params.value}
+        />
+      ),
+    },
+    {
+      field: "subcourtID",
+      headerName: "Court",
+      flex: 2,
+      valueFormatter: (params: GridValueFormatterParams) => {
+        const row: Dispute = params.api.getRow(params.id);
+        if (row){
+            return row.subcourtID.id
         }
-    ];
+        return undefined
+      },
+      renderCell: (params: GridRenderCellParams<Court>) => (
+        <CourtLink
+          chainId={props.chainId}
+          courtId={params.value!.id as string}
+        />
+      ),
+    },
+    {
+      field: "startTime",
+      headerName: "Date",
+      flex: 2,
+      renderCell: (params: GridRenderCellParams<BigNumberish>) =>
+        formatDate(Number(params.value!)),
+    },
+    {
+      field: "txid",
+      headerName: "txID",
+      flex: 1,
+      renderCell: (params: GridRenderCellParams<string>) => (
+        <a
+          href={`${blockExplorer}/txid/${params.value}`}
+          rel="noreferrer"
+          target="_blank"
+        >{`${params.value?.slice(0, 6)}...${params.value?.slice(-4)}`}</a>
+      ),
+    },
+  ];
 
-    return (
-        <Box>
-
-            <Typography sx={{ fontSize: '24px', fontWeight: 600, fontStyle: 'normal', marginTop:'40px' }}>Cases Created:&nbsp; {props.cases? props.cases.length : <Skeleton width={'20px'}/>} </Typography>
-            {<DataGrid
-                sx={{ marginTop: '30px' }}
-                rows={props.cases ? props.cases! : []}
-                columns={dispute_columns}
-                loading={props.isLoading}
-                pageSize={10}
-                disableSelectionOnClick
-                autoHeight={true}
-                hideFooter={false}
-                components={{
-                    Footer: CustomFooter
-                  }}
-            />}
-        </Box>
-
-    )
+  return (
+    <Box>
+      <Typography
+        sx={{
+          fontSize: "24px",
+          fontWeight: 600,
+          fontStyle: "normal",
+          marginTop: "40px",
+        }}
+      >
+        Cases Created:&nbsp;{" "}
+        {props.cases ? props.cases.length : <Skeleton width={"20px"} />}{" "}
+      </Typography>
+      {
+        <DataGrid
+          sx={{ marginTop: "30px" }}
+          rows={props.cases ? props.cases! : []}
+          columns={dispute_columns}
+          loading={props.isLoading}
+          pageSize={10}
+          disableSelectionOnClick
+          autoHeight={true}
+          hideFooter={false}
+          components={{
+            Footer: CustomFooter,
+          }}
+        />
+      }
+    </Box>
+  );
 }
