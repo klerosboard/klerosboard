9ba2deeea540212dd6ea1c371fd84e173cb5d97d
kokialgo <salgozino@gmail.com>
2025-07-08 19:19:33 -0300

    fix: change to production endpoint

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,4 +1,9 @@
-import { ApolloClient, gql, InMemoryCache, NormalizedCacheObject } from "@apollo/client";
+import {
+  ApolloClient,
+  gql,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from "@apollo/client";
 
 const mainnetClient = new ApolloClient({
   uri: "https://api.studio.thegraph.com/query/66145/klerosboard-mainnet/version/latest",
@@ -6,45 +11,62 @@ const mainnetClient = new ApolloClient({
 });
 
 const gnosisClient = new ApolloClient({
-  uri: "https://api.studio.thegraph.com/query/66145/klerosboard-gnosis/version/latest",
+  uri: "https://gateway.thegraph.com/api/subgraphs/id/Ck26N16xgimEuuuNSJqYVWBKcWSwPmkk36BWZGtfx1ox",
   cache: new InMemoryCache(),
+  headers: {
+    "Content-Type": "application/json",
+    // Leaked token, this is protected to be used only by klerosboard.com and this specific subgraph
+    Authorization: "Bearer be8899a2f82bb2a25091d0f5752f2877",
+  },
 });
 
-const curateGnosisClient =  new ApolloClient({
+const curateGnosisClient = new ApolloClient({
   uri: "https://api.studio.thegraph.com/query/61738/legacy-curate-xdai/version/latest",
   cache: new InMemoryCache(),
 });
 
-const curateMainnetClient =  new ApolloClient({
+const curateMainnetClient = new ApolloClient({
   uri: "https://api.studio.thegraph.com/query/61738/legacy-curate-mainnet/version/latest",
   cache: new InMemoryCache(),
 });
 
-
-const apolloClientQuery = async<T>(chainId: string, queryString: string, variables: Record<string, any> = {}) => {
-  if (chainId === '100') return apolloQuery<T>(gnosisClient, queryString, variables);
+const apolloClientQuery = async <T>(
+  chainId: string,
+  queryString: string,
+  variables: Record<string, any> = {}
+) => {
+  if (chainId === "100")
+    return apolloQuery<T>(gnosisClient, queryString, variables);
   return apolloQuery<T>(mainnetClient, queryString, variables);
 };
 
-
-const apolloCurateGnosisQuery = async<T>(queryString: string, variables: Record<string, any> = {}) => {
+const apolloCurateGnosisQuery = async <T>(
+  queryString: string,
+  variables: Record<string, any> = {}
+) => {
   return apolloQuery<T>(curateGnosisClient, queryString, variables);
 };
 
-
-const apolloCurateMainnetQuery = async<T>(queryString: string, variables: Record<string, any> = {}) => {
+const apolloCurateMainnetQuery = async <T>(
+  queryString: string,
+  variables: Record<string, any> = {}
+) => {
   return apolloQuery<T>(curateMainnetClient, queryString, variables);
 };
 
-const apolloQuery = async<T>(client: ApolloClient<NormalizedCacheObject >, queryString: string, variables: Record<string, any> = {}) => {
+const apolloQuery = async <T>(
+  client: ApolloClient<NormalizedCacheObject>,
+  queryString: string,
+  variables: Record<string, any> = {}
+) => {
   try {
     return client.query<T>({
       query: gql(queryString),
-      variables: variables
+      variables: variables,
     });
   } catch (err) {
     console.error("graph ql error: ", err);
   }
 };
 
-export {apolloClientQuery, apolloCurateGnosisQuery, apolloCurateMainnetQuery};
+export { apolloClientQuery, apolloCurateGnosisQuery, apolloCurateMainnetQuery };
